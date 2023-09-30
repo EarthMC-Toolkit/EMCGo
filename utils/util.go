8e0e331192fc7bb7331e6214fe0074a06f400aa7
O.S <owen77stubbs@gmail.com>
2023-09-30 16:55:50 +0100

    wrrrr

--- utils/util.go
+++ utils/util.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"math"
 	"math/rand"
+	"reflect"
 	"strings"
 	"time"
 
@@ -24,27 +25,43 @@ func RandomString(length int) string {
 	return sb.String()
 }
 
-func hypot(num, input, radius int) bool {
+func Hypot(num, input, radius int) bool {
     return num <= (input + radius) && num >= (input - radius)
 }
 
-func manhattan(x1, z1, x2, z2 float64) float64 {
+func Manhattan(x1, z1, x2, z2 float64) float64 {
    return math.Abs(x2 - x1) + math.Abs(z2 - z1)
 }
 
-func euclidean(x1, z1, x2, z2 float64) float64 {
+func Euclidean(x1, z1, x2, z2 float64) float64 {
     return math.Sqrt(math.Pow(x2 - x1, 2) + math.Pow(z2 - z1, 2))
 }
 
-func GetExisting(a1 []any, a2 []string, key string) any {
-	arr := lo.Map(a2, func(x string, index int) any {
-		return nil // TODO: Implement find here
-	})
-
-	if len(arr) > 1 {
-		return arr
-	} else {
-		return arr[0]
-	}
+func GetFieldValue(item interface{}, key string) string {
+    field := reflect.ValueOf(item).FieldByName(key)
+    return lo.Ternary(field.IsValid(), field.String(), "")
 }
 
+type EntityAccess interface {
+	GetName() string
+}
+
+type Entity struct {
+	Name 	string
+}
+
+func (e Entity) GetName() string {
+	return e.Name
+}
+
+func GetExisting[T EntityAccess](a1 []T, a2 []string) any {
+	arr := lo.Map(a2, func(v string, index int) any {
+		val, found := lo.Find(a1, func(el T) bool {
+			return strings.EqualFold(v, el.GetName())
+		})
+
+		return lo.Ternary[any](found, val, nil)
+	})
+
+	return lo.Ternary[any](len(arr) > 1, arr, arr[0])
+}

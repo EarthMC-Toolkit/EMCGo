6878ae9f780b28c54025d4b452927a843e464f8f
O.S <owen77stubbs@gmail.com>
2023-09-30 04:04:23 +0100

    util methods from npm

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module emcgo
 go 1.21.1
 
 require github.com/patrickmn/go-cache v2.1.0+incompatible
+
+require (
+	github.com/samber/lo v1.38.1 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+)
--- utils/util.go
+++ utils/util.go
@@ -1,9 +1,12 @@
 package utils
 
 import (
+	"math"
 	"math/rand"
 	"strings"
 	"time"
+
+	lo "github.com/samber/lo"
 )
 
 var alphabet []rune = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
@@ -20,3 +23,28 @@ func RandomString(length int) string {
 
 	return sb.String()
 }
+
+func hypot(num, input, radius int) bool {
+    return num <= (input + radius) && num >= (input - radius)
+}
+
+func manhattan(x1, z1, x2, z2 float64) float64 {
+   return math.Abs(x2 - x1) + math.Abs(z2 - z1)
+}
+
+func euclidean(x1, z1, x2, z2 float64) float64 {
+    return math.Sqrt(math.Pow(x2 - x1, 2) + math.Pow(z2 - z1, 2))
+}
+
+func GetExisting(a1 []any, a2 []string, key string) any {
+	arr := lo.Map(a2, func(x string, index int) any {
+		return nil // TODO: Implement find here
+	})
+
+	if len(arr) > 1 {
+		return arr
+	} else {
+		return arr[0]
+	}
+}
+

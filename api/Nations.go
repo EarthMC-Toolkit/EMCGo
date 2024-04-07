65c5f4c375ad45c72d61caf6fe2bdb94a6fd58d9
O3H <owen77stubbs@gmail.com>
2024-04-07 10:32:44 +0100

    RRRRRRRRRRRRRRRRRR

--- api/Nations.go
+++ api/Nations.go
@@ -8,18 +8,41 @@ import (
 	"github.com/samber/lo"
 )
 
+type NationList map[string]structs.Nation
+
+func (nations NationList) Get(name string) (*structs.Nation, error) {
+	n, found := nations[name]
+	return &n, lo.Ternary(found, nil, errors.New("Could not find nation with name: "+name))
+}
+
 type Nations struct {
 	MapName string
 }
 
-func (nations *Nations) All() map[string]structs.Nation {
-	mapRes, _ := FetchData(nations.MapName)
-	towns, _ := parser.ParseTowns(mapRes.Sets.Towny)
+func (nations *Nations) All() (NationList, error) {
+	var err error
+	var mapRes structs.MapResponse
 
-	return parser.ParseNations(towns)
+	mapRes, err = FetchData(nations.MapName)
+	if err != nil {
+		return nil, err
+	}
+
+	var towns map[string]structs.Town
+	towns, err = parser.ParseTowns(mapRes.Sets.Towny)
+	if err != nil {
+		return nil, err
+	}
+
+	return parser.ParseNations(towns), nil
 }
 
-func (nations *Nations) Get(name string) (structs.Nation, error) {
-	nation, exists := nations.All()[name]
-	return nation, lo.Ternary(exists, nil, errors.New("Could not find nation with name: "+name))
+func (nations *Nations) Get(name string) (*structs.Nation, error) {
+	all, err := nations.All()
+	if err != nil {
+		return nil, err
+	}
+
+	nation, found := all[name]
+	return &nation, lo.Ternary(found, nil, errors.New("Could not find nation with name: "+name))
 }
--- api/Towns.go
+++ api/Towns.go
@@ -8,16 +8,29 @@ import (
 	"github.com/samber/lo"
 )
 
+type TownList map[string]structs.Town
+
+func (towns TownList) Get(name string) (*structs.Town, error) {
+	t, found := towns[name]
+	return &t, lo.Ternary(found, nil, errors.New("Could not find town with name: "+name))
+}
+
 type Towns struct {
+	TownList
 	MapName string
 }
 
-func (towns *Towns) All() (map[string]structs.Town, error) {
+func (towns *Towns) All() (TownList, error) {
 	mapRes, _ := FetchData(towns.MapName)
 	return parser.ParseTowns(mapRes.Sets.Towny)
 }
 
-func (towns *Towns) Get(name string) (structs.Town, error) {
+func (towns *Towns) Get(name string) (*structs.Town, error) {
 	all, err := towns.All()
-	return all[name], lo.Ternary(err == nil, nil, errors.New("Could not find town with name: "+name))
+	if err != nil {
+		return nil, err
+	}
+
+	town, found := all[name]
+	return &town, lo.Ternary(found, nil, errors.New("Could not find town with name: "+name))
 }
